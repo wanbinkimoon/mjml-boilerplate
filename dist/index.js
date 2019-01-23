54e3d286d27d2e0e58ee8e5afabe5ec7f44eca70
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2019-01-23 17:05:14 +0100

    mjml-components-ok

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,66 @@
 "use strict";
 
-var _base = _interopRequireDefault(require("./base.mjml"));
+var _mjml = _interopRequireDefault(require("mjml"));
+
+var _fs = _interopRequireDefault(require("fs"));
+
+var _path = _interopRequireDefault(require("path"));
+
+var _mjmlCore = require("mjml-core");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-console.log(_base.default); // STEP: appending the compiled html
+// NOTE: path mapping
+var pathToSrc = _path.default.join(process.cwd(), '/src/');
 
-document.open('text/html');
-document.write(_base.default);
-document.close();
-console.log(document);
+var pathToComponents = _path.default.join(process.cwd(), '/dist/components');
+
+var pathToMjmlbase = _path.default.join(process.cwd(), '/src/base.mjml');
+
+var pathToHtmlResult = _path.default.join(process.cwd(), '/dist/index.html'); // STEP: getting the component list
+
+
+var walkSync = function walkSync(dir) {
+  var filelist = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+
+  _fs.default.readdirSync(dir).forEach(function (file) {
+    filelist = _fs.default.statSync(_path.default.join(dir, file)).isDirectory() ? walkSync(_path.default.join(dir, file), filelist) : filelist.concat(_path.default.join(dir, file));
+  });
+
+  return filelist;
+};
+
+var components = walkSync(pathToComponents); // STEP: rgistering the components
+
+components.forEach(function (d) {
+  console.log(d);
+  (0, _mjmlCore.registerComponent)(require(d).default);
+}); // STEP: configuring the mjml compiler
+
+var options = {
+  fonts: {},
+  keepComments: true,
+  beautify: true,
+  minify: true,
+  validationLevel: 'strinct'
+};
+
+function compileMJML() {
+  // STEP: getting the mjml codebase
+  _fs.default.readFile(pathToMjmlbase, 'utf8', function (err, data) {
+    // CHECK: for errors
+    if (err) throw err; // STEP: compiling the mjml code
+
+    var output = (0, _mjml.default)(data, options); // STEP: writing a new html file
+
+    _fs.default.writeFile(pathToHtmlResult, output.html, function (err) {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    });
+  });
+} // STEP: watch files
+
+
+_fs.default.watch(pathToSrc, function () {
+  return compileMJML();
+});
--- src/index.js
+++ src/index.js
@@ -1,9 +1,57 @@
-import base from './base.mjml';
-console.log(base);
+import mjml2html from 'mjml';
+import fs from 'fs';
+import path from 'path';
+import {registerComponent} from 'mjml-core';
 
-// STEP: appending the compiled html
-document.open('text/html');
-document.write(base);
-document.close();
+// NOTE: path mapping
+const pathToSrc = path.join(process.cwd(), '/src/');
 
-console.log(document);
+const pathToComponents = path.join(process.cwd(), '/dist/components');
+const pathToMjmlbase = path.join(process.cwd(), '/src/base.mjml');
+const pathToHtmlResult = path.join(process.cwd(), '/dist/index.html');
+
+// STEP: getting the component list
+const walkSync = (dir, filelist = []) => {
+  fs.readdirSync(dir).forEach(file => {
+    filelist = fs.statSync(path.join(dir, file)).isDirectory()
+      ? walkSync(path.join(dir, file), filelist)
+      : filelist.concat(path.join(dir, file));
+  });
+  return filelist;
+};
+const components = walkSync(pathToComponents);
+
+// STEP: rgistering the components
+components.forEach(d => {
+  console.log(d);
+  registerComponent(require(d).default);
+});
+
+// STEP: configuring the mjml compiler
+const options = {
+  fonts: {},
+  keepComments: true,
+  beautify: true,
+  minify: true,
+  validationLevel: 'strinct',
+};
+
+function compileMJML() {
+  // STEP: getting the mjml codebase
+  fs.readFile(pathToMjmlbase, 'utf8', (err, data) => {
+    // CHECK: for errors
+    if (err) throw err;
+
+    // STEP: compiling the mjml code
+    const output = mjml2html(data, options);
+
+    // STEP: writing a new html file
+    fs.writeFile(pathToHtmlResult, output.html, err => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    });
+  });
+}
+
+// STEP: watch files
+fs.watch(pathToSrc, () => compileMJML());
